418baa28629d64bfd5ecd3079e75411910fe9451
Boom <ap1071.bu.it@gmail.com>
2026-03-18 16:46:06 +0700

    [Edited]: Setting ก่อนนำขึ้น cloud

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🌍 URL: http://localhost:${PORT}`);
 });
