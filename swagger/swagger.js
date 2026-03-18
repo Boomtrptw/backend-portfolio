9fc33b83c43ebbaea5bd69d768ef4a0b124dad16
Boom <ap1071.bu.it@gmail.com>
2026-03-18 17:15:01 +0700

    อีกรอบ

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -11,9 +11,14 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000"
-      }
-    ]
+        url: "https://boom-portfolio-backend.onrender.com", // เพิ่ม URL Render ของคุณบูมตรงนี้
+        description: "Production Server (Render)",
+      },
+      {
+        url: "http://localhost:5000",
+        description: "Local Development Server",
+      },
+    ],
   },
   // ใช้ path.join เพื่อความแม่นยำ ไม่ว่าสคริปต์จะรันจากโฟลเดอร์ไหน
   apis: [
